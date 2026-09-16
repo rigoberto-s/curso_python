5899255a8d3c9ab62bacea44b40a623b0a85defc
Rigoberto Sanchez <rigobertosanchez1817@gmail.com>
2026-09-16 15:35:49 -0600

    hello world

---
+++ curso Andres/1-hello-world/main.py
@@ -0,0 +1,9 @@
+def sayHello(name):
+    print('Este scrip solo se ejecuta si el scrip es ejecutado de forma directa')
+    print(f'Hola {name}')
+
+
+if __name__ == '__main__':
+    print('Hello world')
+    sayHello('Pepe')
+
---
+++ curso Andres/1-hello-world/other.py
@@ -0,0 +1,7 @@
+import main
+
+
+user = 'Luis Guapo'
+print(f'Hola que tal {user}')
+
+main.sayHello('Jose jose')
