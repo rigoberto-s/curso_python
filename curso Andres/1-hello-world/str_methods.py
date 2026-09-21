c46972cbbaf0602a6ac508fdf10a3cdc25217864
Rigoberto Sanchez <rigobertosanchez1817@gmail.com>
2026-09-20 22:55:35 -0600

    metodos de string

--- curso Andres/1-hello-world/str_methods.py
+++ curso Andres/1-hello-world/str_methods.py
@@ -21,7 +21,7 @@ print(txt.rstrip())
 text = 'Hola java porfin'
 print(text.replace('java','python'))
 
-print('------------------------------------------------')
+print('---------------------------------------------------------------------')
 text = 'Andrez,Guzman,Jose,java'
 data_list = text.split(",")#por que se puso un coma aqui
 print(data_list[1])
@@ -30,3 +30,20 @@ print(data_list)
 lista = ["Andrez", "Guzman", "Jose", "java"]
 text = ' '.join(lista)#dejamos un espacio para que por cada palabra se deje un espacio
 print(text)
+
+print('-------------------------------------------------------------------------------------------------------')
+
+texto = 'Hola, Getsai premiun es el pirata'
+print(texto.find('Getsai'))# aqui me sale en consola que es la pocision 6 pero solo busca donde esta la primer letra?
+print(texto.index('el'))#la unica diferencia es que si index no encunetra lanza exception
+
+
+print(texto.startswith('Hola,'))
+print(texto.endswith('pirata'))
+
+number = '1234'
+decimal = '1234.45'
+parraf = 'c#'
+mix = 'java jdk 17'
+
+print(number.isnumeric())
